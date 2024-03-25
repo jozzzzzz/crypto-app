f1caa7e8bcb90d3f79b6fc74d55419b6e45d1a26
Moudjane <mathis.oudjane@gmail.com>
2024-03-25 11:55:48 +0100

    change axios to fetch

--- src/classObject/CryptoGraph.js
+++ src/classObject/CryptoGraph.js
@@ -1,23 +1,26 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 
 function CryptoGraph(symbol) {
-    const [data, setData] = useState(null);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=1d`);
-          setData(response.data);
-        } catch (error) {
-          console.error('Error fetching data: ', error);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=1d`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
-      };
-  
-      fetchData();
-    }, [symbol]);
-  
-    return data;
-  }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+  }, [symbol]);
+
+  return data;
+}
 
 export default CryptoGraph;
